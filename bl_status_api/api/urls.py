756269b9e3964d086e6fa314b9d0b4661d5639b5
Edward Bradley <edbrad@edbrad.com>
2017-08-06 14:46:44 -0500

    CRUD Endpoints in place

--- bl_status_api/api/urls.py
+++ bl_status_api/api/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     url(r'^status-count/$', views.status_count, name='status_count'),
     url(r'^find-one-by-pattern/$', views.find_one_by_pattern, name='find_one_by_pattern'),
     url(r'^find-many-by-pattern/$', views.find_many_by_pattern, name='find_many_by_pattern'),
+    url(r'^insert-one/$', views.insert_one, name='insert_one'),
+    url(r'^insert-many/$', views.insert_many, name='insert_many'),
+    url(r'^update-one/$', views.update_one, name='update_one'),
+    url(r'^delete-one-by-pattern/$', views.delete_one_by_pattern, name='delete_one_by_pattern'),
+    url(r'^delete-many-by-pattern/$', views.delete_many_by_pattern, name='delete_many_by_pattern'),
     url(r'^docs/', include('rest_framework_docs.urls')),
 ]
--- bl_status_api/api/views.py
+++ bl_status_api/api/views.py
@@ -2,24 +2,28 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import parser_classes
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from bson import json_util
 from django.http import HttpResponse
-import re
+from bson.objectid import ObjectId
+
+import re # Regular Expression support
+import ast # Literal Evaluation support (string to dictionary)
 
 # Global variables
 mongo_server_connection = 'mongodb://172.16.168.110:27017/' # prod db server
 database = 'test_database' # test database
 collection = 'status_test' # test collection (table)
 
-# API Default/Welcome Response
+# API Default/Welcome Text Response
 @api_view(['GET'])
 def root(request):
     """
-    Return Generic/Default Text Response.
+    Return Generic/Default Welcome Text Response.
     """
-    data = 'Welcome to the EMS bl-status REST API'
+    data = 'Welcome to the EMS bl-status REST API. Unauthorized Access is Prohibited. 2017 - Executive Mailing Service'
     return HttpResponse(data)
 
 # Get All Status Records/Documents
@@ -37,17 +41,17 @@ def all_statuses(request):
         data = '[{"database error":' + e + '}]'
         return JsonResponse(data, content_type='application/json')
 
-    # Get all the status records in the database and return to client
+    # Get all the status records in the database and return to client (w/ JSON Serialization)
     db = connection[database]
     data = json_util.dumps(db[collection].find({}))
     connection.close()
     return HttpResponse(data, content_type='application/json')
 
-# Get Status Record/Document Count
+# Get Status Document (Record) Count
 @api_view(['GET'])
 def status_count(request):
     """
-    Return Status Document Count
+    Return Current Status Document (Record) Count
     """
     data = [] # JSON data array (query results)
 
@@ -64,11 +68,11 @@ def status_count(request):
     connection.close()
     return HttpResponse(data, content_type='application/json')
 
-# Find/Get A Single Document By Pattern
+# Find/Get A Single Document By Pattern Code
 @api_view(['GET'])
 def find_one_by_pattern(request):
     """
-    Find And Return A Single Status Document Using Pattern (9999-99X)
+    Find and Return a Single Status Document by Pattern Code (9999-99X)
     """
     try:
         pattern = request.GET['pattern']  # pattern to search for
@@ -84,7 +88,7 @@ def find_one_by_pattern(request):
         data = '[{"database error":' + e + '}]'
         return HttpResponse(data, content_type='application/json')
 
-    # Find a document database with the matching pattern and return to client
+    # Find a document database with the matching pattern and return to client (w/ JSON Serialization)
     db = connection[database]
     if (pattern != ""):
         data = json_util.dumps(db[collection].find_one({"pattern": pattern}))
@@ -94,11 +98,11 @@ def find_one_by_pattern(request):
     connection.close()
     return HttpResponse(data, content_type='application/json')
 
-# Find/Get Multiple Documents By Partial Pattern
+# Find/Get Multiple Documents By Partial Pattern Code
 @api_view(['GET'])
 def find_many_by_pattern(request):
     """
-    Find And Return Multiple Status Documents Using Partial Pattern (999*)
+    Find and Return Multiple Status Documents by Partial Pattern Code (999*)
     """
     try:
         pattern = request.GET['pattern']  # pattern to search for
@@ -114,7 +118,7 @@ def find_many_by_pattern(request):
         data = '[{"database error":' + e + '}]'
         return HttpResponse(data, content_type='application/json')
 
-    # Find the documents in the database where the pattern starts with "x" and return to client
+    # Find the documents in the database where the pattern starts with "x" and return to client (w/ JSON Serialization)
     db = connection[database]
     if (pattern != ""):
         search_pattern = "^" + pattern
@@ -125,18 +129,19 @@ def find_many_by_pattern(request):
     connection.close()
     return HttpResponse(data, content_type='application/json')
 
-# Delete A Single Document By Pattern
+# Delete A Single Document By Pattern Code
 @api_view(['DELETE'])
 def delete_one_by_pattern(request):
     """
-    Delete A Single Status Document Using Pattern (9999-99X)
+    Delete a Single Status Document by Pattern Code (9999-99X) and Return a Result Object
     """
+
     try:
-        pattern = request.GET['pattern']  # pattern to search for
+        pattern = request.GET['pattern']  # pattern code to search for
     except Exception:
         pattern = ""
 
-    data = [] # JSON data array (query results)
+    data = "" #  result object
 
     # Verify the connection
     try:
@@ -145,12 +150,153 @@ def delete_one_by_pattern(request):
         data = '[{"database error":' + e + '}]'
         return HttpResponse(data, content_type='application/json')
 
-    # Find a document database with the matching pattern and return to client
+    # Delete a single document database with the matching pattern code and return a result object to client
     db = connection[database]
     if (pattern != ""):
-        data = json_util.dumps(db[collection].find_one({"pattern": pattern}))
+        delete_result  = db[collection].delete_one({"pattern": pattern})
+        data = delete_result.raw_result
     else:
-        data = [{}]
+        data = ""
 
     connection.close()
-    return HttpResponse(data, content_type='application/json')
+    return Response(data)
+
+# Delete All Matching Documents By A Partial Pattern Code
+@api_view(['DELETE'])
+def delete_many_by_pattern(request):
+    """
+    Delete All Documents by a Partial Pattern Code (999*) and Return a Result Object
+    """
+    try:
+        pattern = request.GET['pattern']  # pattern to delete by
+    except Exception:
+        pattern = ""
+
+    data = "" #  result object
+
+    # Verify the connection
+    try:
+        connection = MongoClient(mongo_server_connection, serverSelectionTimeoutMS=30)
+    except Exception as e:
+        data = '[{"database error":' + e + '}]'
+        return HttpResponse(data, content_type='application/json')
+
+    # Delete the documents in the database where the pattern code starts with "x" 
+    db = connection[database]
+    if (pattern != ""):
+        search_pattern = "^" + pattern
+        delete_result = db[collection].delete_many({ "pattern": re.compile(search_pattern, re.IGNORECASE)})
+        data = delete_result.raw_result
+    else:
+        data = ""
+
+    connection.close()
+    return Response(data)
+
+# Insert A Single New Status Document
+@api_view(['POST'])
+def insert_one(request):
+    """
+    Insert a Single New Status Document and Return the New id
+    """
+    try:
+        status_data = request.data
+    except Exception:
+        status_data = ""
+
+    id = "" # new id
+
+    # Verify the connection
+    try:
+        connection = MongoClient(mongo_server_connection, serverSelectionTimeoutMS=30)
+    except Exception as e:
+        data = '[{"database error":' + e + '}]'
+        return HttpResponse(data, content_type='application/json')
+    
+    # Insert the new document
+    db = connection[database]
+    if (status_data != ""):
+        id = json_util.dumps(db[collection].insert_one(status_data).inserted_id)
+    else:
+        id = ""
+    
+    connection.close()
+
+    # Convert response to dictionary (for JSON response serialization)
+    id_dict = ast.literal_eval(id)
+    oid = id_dict['$oid']
+
+    return Response({'inserted_id': oid })
+    
+
+# Insert Many New Status Documents
+@api_view(['POST'])
+def insert_many(request):
+    """
+    Insert Many New Status Documents and Return an Array of the New id's
+    """
+    try:
+        status_data = request.data
+    except Exception:
+        status_data = ""
+
+    ids = "" # new id's
+
+    # Verify the connection
+    try:
+        connection = MongoClient(mongo_server_connection, serverSelectionTimeoutMS=30)
+    except Exception as e:
+        data = '[{"database error":' + e + '}]'
+        return HttpResponse(data, content_type='application/json')
+    
+    # Insert the new documents
+    db = connection[database]
+    if (status_data != ""):
+        ids = json_util.dumps(db[collection].insert_many(status_data).inserted_ids)
+    else:
+        ids = ""
+    
+    connection.close()
+
+    # Convert response (array of inserted id's) to dictionary list (for JSON response serialization)
+    ids_dict = ast.literal_eval(ids)
+    oids = []
+    for id in ids_dict:
+        oids.append(id['$oid'])
+
+    return Response({'inserted_ids': oids })
+
+# Update A Single Status Document
+@api_view(['PUT'])
+def update_one(request):
+    """
+    Update a Single Status Document and Return a Result Object
+    """
+    try:
+        status_data = request.data
+    except Exception:
+        status_data = ""
+
+    update_id = status_data['update_id']
+    update_data = status_data['update_data']
+
+    print(update_id)
+    print(update_data)
+
+    # Verify the connection
+    try:
+        connection = MongoClient(mongo_server_connection, serverSelectionTimeoutMS=30)
+    except Exception as e:
+        data = '[{"database error":' + e + '}]'
+        return HttpResponse(data, content_type='application/json')
+    
+    # Update the document and return the Result Object
+    db = connection[database]
+    if (status_data != ""):
+        update_object = db[collection].update_one({'_id' : ObjectId(str(update_id))}, {'$set': update_data}, upsert=False)
+        print(update_object.raw_result)
+    else:
+        ids = ""
+    
+    connection.close()
+    return Response(update_object.raw_result)
